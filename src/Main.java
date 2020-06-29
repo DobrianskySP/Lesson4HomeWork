3595bb8fbe08fab25a4836d78ca82af206562b00
Станислав <dobriansky.sp@oilpc.ru>
2020-06-29 17:36:44 +0300

    Its working!

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,41 @@
 public class Main {
-    public static void main(String[] args) {
-        MyThread t1 = new MyThread("1");
-        //MyThread t2 = new MyThread("2");
-       //MyThread t3 = new MyThread("3");
+        private static Object mon = new Object();
+        private static char curLet = 'A';
 
-       // t1.printLetterA();
-       // t2.printLetterB();
-       // t3.printLetterC();
+    public static void main(String[] args) {
+        //Main t1 = new Main();
+        MyThread t1 = new MyThread();
+        MyThread t2 = new MyThread();
+        MyThread t3 = new MyThread();
+
+       // t1.printLetter('A','B');
+      //  t1.printLetter('B','C');
+       // t1.printLetter('C','A');
+
+       new Thread(() -> t1.printLetter('A','B') ).start();
+       new Thread(() -> t2.printLetter('B','C') ).start();
+       new Thread(() -> t3.printLetter('C','A') ).start();
+
+
 
    }
+
+    public void printLetter(char c , char nextLet) {
+           //System.out.println("Запущен поток печати буквы " + curLet);
+        synchronized (mon) {
+            for (int i = 0; i < 5; i++) {
+                try {
+                    while (Main.curLet != c) {
+                        //System.out.println("Вызван метод wait  в потоке " + curLet);
+                        mon.wait();
+                    }
+                    System.out.print(curLet);
+                    curLet = nextLet;
+                    mon.notifyAll();
+                    } catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
 }
--- src/MyThread.java
+++ src/MyThread.java
@@ -1,74 +1,28 @@
 public class MyThread extends Thread {
-    static char letter = 'a';
+    static char curLet = 'A';
     static Object mon = new Object();
-
-    public MyThread(String name) {
-        System.out.println("Поток с именем " + name);
-        start();
-    }
 
     @Override
     public void run() {
-        printLetterA();
-        printLetterB();
-        printLetterC();
     }
 
-    public void printLetterA() {
-        System.out.println("Запущен поток печати буквы а");
+    public void printLetter(char c , char nextLet) {
+        System.out.println("Запущен поток печати буквы " + curLet);
         synchronized (mon) {
             for (int i = 0; i < 5; i++) {
-                while (letter != 'a') {
-                    try {
-                        System.out.println("Вызван метод wait  в потоке а");
+                try {
+                    while (curLet != c) {
+                        System.out.println("Вызван метод wait  в потоке " + curLet);
                         mon.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
                     }
-                }
-                    System.out.println(letter);
-                    letter = 'b';
+                    System.out.println(c);
+                    curLet = nextLet;
                     mon.notifyAll();
-            }
-            System.out.println("+");
-        }
-    }
-
-    public void printLetterB() {
-        System.out.println("Запущен поток печати буквы b");
-        synchronized (mon) {
-            for (int i = 0; i < 5; i++) {
-                while (letter != 'b') {
-                    try {
-                        System.out.println("Вызван метод wait  в потоке b");
-                        mon.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
                 }
-                System.out.println(letter);
-                letter = 'c';
-                mon.notifyAll();
             }
         }
     }
 
-    public void printLetterC() {
-        System.out.println("Запущен поток печати буквы c");
-        synchronized (mon) {
-            for (int i = 0; i < 5; i++) {
-                while (letter != 'c') {
-                    try {
-                        System.out.println("Вызван метод wait  в потоке c");
-                        mon.wait();
-                    } catch (InterruptedException e) {
-                        e.printStackTrace();
-                    }
-                }
-                System.out.println(letter);
-                letter = 'a';
-                mon.notifyAll();
-            }
-        }
-    }
 }
